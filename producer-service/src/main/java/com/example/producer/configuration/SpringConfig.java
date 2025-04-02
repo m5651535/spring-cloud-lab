b6c3aeca9bcff276610cd3416e159a9e1339ffdb
DrakeXu <drake.xu@sunbirddcim.com>
2025-04-02 17:58:08 +0800

    try to use config to replace local application.yml but fail

--- producer-service/src/main/java/com/example/producer/configuration/SpringConfig.java
+++ producer-service/src/main/java/com/example/producer/configuration/SpringConfig.java
@@ -1,8 +1,13 @@
 package com.example.producer.configuration;
 
+import com.ecwid.consul.v1.ConsulClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.consul.config.ConsulConfigProperties;
+import org.springframework.cloud.consul.config.ConsulPropertySourceLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,5 +16,11 @@ public class SpringConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @Primary
+    public ConsulPropertySourceLocator consulPropertySourceLocator(ConsulClient consulClient, ConsulConfigProperties properties) {
+        return new ConsulPropertySourceLocator(consulClient, properties);
     }
 }
